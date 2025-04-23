8af1b26da5b2f4e5eed3b2f12bdbc1470f09030c
Joshua Emilola <jemilola@miu.edu>
2025-04-23 04:50:37 -0500

    reconfigure OAI

--- infra/lib/services/cloudfront.ts
+++ infra/lib/services/cloudfront.ts
@@ -1,5 +1,4 @@
 import { Construct } from 'constructs';
-import * as iam from 'aws-cdk-lib/aws-iam';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
@@ -8,36 +7,21 @@ export function createCloudFrontDistro(
   scope: Construct,
   siteBucket: s3.Bucket,
 ): cloudfront.Distribution {
-  const originAccessControl = new cloudfront.CfnOriginAccessControl(scope, 'SiteOAC', {
-    originAccessControlConfig: {
-      name: 'StaticSiteOAC',
-      signingBehavior: 'always',
-      signingProtocol: 'sigv4',
-      originAccessControlOriginType: cloudfront.OriginAccessControlOriginType.S3,
-
-    }
+  const originAccessIdentity = new cloudfront.OriginAccessIdentity(scope, 'OAI', {
+    comment: 'OAI for CloudFront access to S3',
   });
 
-  const distribution = new cloudfront.Distribution(scope, 'SiteDistribution', {
+  siteBucket.grantPublicAccess();
+  siteBucket.grantRead(originAccessIdentity);
+  
+  return new cloudfront.Distribution(scope, 'SiteDistribution', {
     defaultBehavior: {
       origin: new origins.S3Origin(siteBucket, {
-        originAccessControlId: originAccessControl.attrId,
+        originAccessIdentity,
       }),
       viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
     },
+
     defaultRootObject: 'index.html',
   });
-
-  siteBucket.addToResourcePolicy(new iam.PolicyStatement({
-    actions: ['s3:GetObject'],
-    resources: [`${siteBucket.bucketArn}/*`],
-    principals: [new iam.ServicePrincipal('cloudfront.amazonaws.com')],
-    conditions: {
-      StringEquals: {
-        'AWS:SourceArn': distribution.distributionArn,
-      },
-    },
-  }));
-
-  return distribution;
 }
